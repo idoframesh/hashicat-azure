b8cb080b7c71cbcbd9a29c2d9231b4d844570e99
Ramesh Joshi <rameshchandra.joshi@fisglobal.com>
2021-08-20 18:39:11 +0000

    added vnet module4

--- vnet.tf
+++ vnet.tf
@@ -1,5 +1,5 @@
 module "network" {
-  source  = "private/rbjoshi-training/network/azurerm/3.0.1"
+  source  = "Azure/network/azurerm"
   version = "3.0.1"
   # insert required variables here
   resource_group_name = azurerm_resource_group.myresourcegroup.name
