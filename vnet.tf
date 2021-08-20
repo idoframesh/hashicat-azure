1f8282a8db9720a7e844cb6b65791c68b60a8bbf
Ramesh Joshi <rameshchandra.joshi@fisglobal.com>
2021-08-20 18:17:00 +0000

    added vnet module1

--- vnet.tf
+++ vnet.tf
@@ -1,5 +1,5 @@
 module "network" {
-  source  = "app.terraform.io/dom-org/network/azurerm"
+  source  = "app.terraform.io/app/rbjoshi-training/registry/modules/private/rbjoshi-training/network/azurerm/"
   version = "3.0.1"
   # insert required variables here
   resource_group_name = azurerm_resource_group.myresourcegroup.name
